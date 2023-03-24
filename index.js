ed6ce980cf245869d88380dca39c1d807e0f161d
Sharonne24 <sawuor395@gmail.com>
2023-03-24 22:56:10 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+
+
+function moveDodgerRight() {
+    const dodger = document.getElementById('dodger');
+
+    const currentLeft = parseInt(dodger.style.left.replace('px', ''));
+    const newLeft = Math.min(currentLeft + 10, window.innerWidth - 40);
+  
+    dodger.style.left = `${newLeft}px`;
+
+  }
+  
+  
